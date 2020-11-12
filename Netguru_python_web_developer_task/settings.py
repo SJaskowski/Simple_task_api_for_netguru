fae83e9a79d0b152c00fc6e66dde85399f382705
SJaskowski <34011723+SJaskowski@users.noreply.github.com>
2020-11-12 22:03:48 +0100

    Update settings.py

--- Netguru_python_web_developer_task/settings.py
+++ Netguru_python_web_developer_task/settings.py
@@ -121,6 +121,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 REST_FRAMEWORK = {'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema'}
